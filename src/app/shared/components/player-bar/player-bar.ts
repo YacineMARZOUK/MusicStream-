070cda884871d96777c3d60506ef630b399f591a
YacineMARZOUK <yacinemarzou@gmail.com>
2026-01-10 15:59:20 +0100

    Generate core services and components structure

---
+++ src/app/shared/components/player-bar/player-bar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-bar',
+  imports: [],
+  templateUrl: './player-bar.html',
+  styleUrl: './player-bar.css',
+})
+export class PlayerBar {
+
+}
